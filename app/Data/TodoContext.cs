a70d86d62323e40f69e7fd1c5590351dca0e0741
Akhilesh Boodhun <akhilesh.boodhun@umail.uom.ac.mu>
2021-12-21 22:59:38 +0400

    First commit

---
+++ app/Data/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using app.Models;
+
+namespace app.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext (DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<app.Models.Todo> Todo { get; set; }
+    }
+}
